eb9cb1d47726ceb426f33c2720644a95fdde9073
Amullen45 <112916059+Amullen45@users.noreply.github.com>
2022-11-25 16:27:29 -0500

    Library form Selection JS

---
+++ js/libraryformselector.js
@@ -0,0 +1,47 @@
+var subjectObject = {
+    "Chest": {
+      "Upper",
+      "Middle",
+      "Lower",
+      "Misc",   
+    },
+    "Back": {
+        "Upper",
+        "Middle",
+        "Lower",
+    },
+    "Arms": {
+      "Bicep",
+      "Tricep",
+      "Forearm",
+    },
+    "Legs": {
+        "Quads",
+        "Glutes",
+        "Hamstring",
+    },
+    "Shoulders": {
+        "Anterior",
+        "Lateral",
+        "Posterior",
+    },
+    "Calves"
+  }
+  window.onload = function() {
+    //subjectSel
+    var mGroupSel = document.getElementById("mgroup");
+    //topicSel
+    var sGroupSel = document.getElementById("sgroup");
+
+    for (var x in subjectObject) {
+      mGroupSel.options[mGroupSel.options.length] = new Option(x, x);
+    }
+    mGroupSel.onchange = function() {
+      //empty Chapters- and Topics- dropdowns
+      sGroupSel.length = 1;
+      //display correct values
+      for (var y in subjectObject[this.value]) {
+        sGroupSel.options[sGroupSel.options.length] = new Option(y, y);
+      }
+    }
+    }
