5fdce1d07549c1658a8e18847f4a57edcdd046ab
Andrew Mullen <Andrewjmullen45@gmail.com>
2022-11-27 20:50:00 -0500

    Typo's and CSS update

--- js/libraryformselector.js
+++ js/libraryformselector.js
@@ -1,31 +1,31 @@
 var subjectObject = {
-    "Chest": [
-        "Upper",
-      "Middle",
-      "Lower",
-      "Misc"  
-    ],
-    "Back": [
-        "Upper",
-        "Middle",
-        "Lower"
-    ],
-    "Arms": [
-      "Bicep",
-      "Tricep",
-      "Forearm"
-    ],
-    "Legs": [
-        "Quads",
-        "Glutes",
-        "Hamstring"
-    ],
-    "Shoulders": [
-        "Anterior",
-        "Lateral",
-        "Posterior"
-    ],
-    "Calves"
+    "Chest": {
+        "Upper":[],
+      "Middle":[],
+      "Lower":[],
+      "Misc":[] 
+    },
+    "Back": {
+        "Upper":[],
+        "Middle":[],
+        "Lower":[]
+    },
+    "Arms": {
+      "Bicep":[],
+      "Tricep":[],
+      "Forearm":[]
+    },
+    "Legs": {
+        "Quads":[],
+        "Glutes":[],
+        "Hamstring":[]
+    },
+    "Shoulders": {
+        "Anterior":[],
+        "Lateral":[],
+        "Posterior":[]
+    },
+    "Calves":[]
   };
   window.onload = function() {
 
@@ -36,8 +36,8 @@ var subjectObject = {
       mGroupSel.options[mGroupSel.options.length] = new Option(x, x);
     }
     mGroupSel.onchange = function() {
-      //empty Chapters- and Topics- dropdowns
-      sGroupSel.length = 1;
+      //empty Chapters- and Topics- dropdowns
+      sGroupSel.length = 1;
       //display correct values
       for (var y in subjectObject[this.value]) {
         sGroupSel.options[sGroupSel.options.length] = new Option(y, y);
